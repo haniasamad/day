5ef5bc17b810424b998abef5a4843757934717bc
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-10-17 20:06:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// let days = ["sunday", "Monday", "tuesday", "Wednesday" ,"Thursday", "friday", "saturday"]
+// let date = new Date();
+// let day = date.getDay()
+// console.log(days[day]);
+// function tellTime(){
+//     let time = new Date();
+//     alert("ok")
+    
+// }
+ function tellTime() {
+     var now = new Date();
+     var theHr = now.getHours();
+     var theMin = now.getMinutes();
+     alert("Current time: "+ theHr + ":" + theMin);
+ }
+  
+
+
+  
